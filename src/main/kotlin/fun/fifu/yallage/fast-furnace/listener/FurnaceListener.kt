e4aaeebfd4da06b4b2936767905d83c6ec28dfdc
NekokeCore <core2002@163.com>
2022-03-16 14:18:28 +0800

    fix some bug and add some TODO

--- src/main/kotlin/fun/fifu/yallage/fast-furnace/listener/FurnaceListener.kt
+++ src/main/kotlin/fun/fifu/yallage/fast-furnace/listener/FurnaceListener.kt
@@ -13,6 +13,7 @@ import org.bukkit.event.inventory.InventoryOpenEvent
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.event.player.PlayerChatEvent
 import org.bukkit.inventory.FurnaceInventory
+import org.bukkit.inventory.InventoryView
 import org.bukkit.inventory.ItemStack
 
 /**
@@ -35,8 +36,8 @@ class FurnaceListener : Listener {
 
     @EventHandler
     fun onInvOpen(event: InventoryOpenEvent) {
-        val inventory = event.inventory
-        if (inventory is FurnaceInventory && inventory.holder!!.isFastFurnace()) {
+        if (event.view.isFastFurnace()) {
+            event.player.sendMessage("你打开了一个快速熔炉")
             event.player.closeInventory()
             val createInventory = Bukkit.createInventory(event.inventory.holder, InventoryType.FURNACE, "快速熔炉")
             event.player.openInventory(createInventory)
@@ -45,11 +46,11 @@ class FurnaceListener : Listener {
 
     @EventHandler
     fun onInvClick(event: InventoryClickEvent) {
-        if (event.inventory.holder is Furnace && (event.inventory.holder as Furnace).isFastFurnace()) {
+        if (event.view.isFastFurnace()) {
             val holder = event.inventory.holder as Furnace
             if (holder.cookTimeTotal != 0) {
                 holder.cookTimeTotal = 0
-                holder.decrease()
+                holder.block.decrease()
             }
         }
     }
@@ -57,16 +58,26 @@ class FurnaceListener : Listener {
 
     @EventHandler
     fun onFastFurnacePlace(event: BlockPlaceEvent) {
-        if (event.block is Furnace && event.itemInHand.isFastFurnace()) {
+        val block = event.block
+        if (block.type == Material.FURNACE && event.itemInHand.isFastFurnace()) {
             fastFurnaceMap[event.block.toTriple()] = event.itemInHand.getTheDurability()
+//            event.player.sendMessage("你在${event.block.toTriple()}放置了一个快速熔炉")
         }
     }
 
     @EventHandler
     fun onFastFurnaceBreak(event: BlockBreakEvent) {
         val block = event.block
-        if (block is Furnace && block.isFastFurnace()) {
-            block.getTheDurability()?.let { event.player.inventory.itemInMainHand.setTheDurability(it) }
+        if (block.type == Material.FURNACE && block.isFastFurnace()) {
+            val itemStack = ItemStack(Material.FURNACE)
+            val im = itemStack.itemMeta
+            im!!.lore = arrayListOf("快速熔炉", "4")
+            itemStack.itemMeta = im
+//TODO
+
+            fastFurnaceMap.remove(block.toTriple())
+
+//            event.player.sendMessage("你在${event.block.toTriple()}拆除了一个快速熔炉")
         }
     }
 
@@ -74,14 +85,19 @@ class FurnaceListener : Listener {
      * Return if the furnace is a  Fast Furnace
      * @return true or false
      */
-    fun Furnace.isFastFurnace() = fastFurnaceMap.contains(Triple(x, y, z))
+    fun Block.isFastFurnace() = fastFurnaceMap.contains(Triple(x, y, z))
 
+    /**
+     * Return if the furnace is a  Fast Furnace
+     * @return true or false
+     */
+    fun InventoryView.isFastFurnace() = this is FurnaceInventory && title == "快速熔炉"
 
     /**
      * Return if the item stack is a Fast Furnace
      * @return true or false
      */
-    fun ItemStack.isFastFurnace() = type != Material.FURNACE && itemMeta?.lore?.get(0) == "快速熔炉"
+    fun ItemStack.isFastFurnace() = type == Material.FURNACE && itemMeta?.lore?.get(0) == "快速熔炉"
 
     /**
      * Returns the durability of the Fast Furnace
@@ -106,7 +122,7 @@ class FurnaceListener : Listener {
      * Returns the durability of the Fast Furnace
      * @return durability
      */
-    fun Furnace.getTheDurability(): Int? {
+    fun Block.getTheDurability(): Int? {
         if (!isFastFurnace())
             throw RuntimeException("The type being operated is not a Fast Furnace")
         return fastFurnaceMap[Triple(x, y, z)]
@@ -115,7 +131,7 @@ class FurnaceListener : Listener {
     /**
      * Set the durability of the Fast Furnace
      */
-    fun Furnace.setTheDurability(dur: Int) {
+    fun Block.setTheDurability(dur: Int) {
         if (!isFastFurnace())
             throw RuntimeException("The type being operated is not a Fast Furnace")
         fastFurnaceMap[Triple(x, y, z)] = dur
@@ -129,14 +145,22 @@ class FurnaceListener : Listener {
     /**
      * Set the durability -1 of the Fast Furnace
      */
-    fun Furnace.decrease() {
+    fun Block.decrease() {
         val dur = getTheDurability()!! - 1
         if (dur > 0)
             setTheDurability(dur)
         else {
             setTheDurability(0)
             fastFurnaceMap.remove(Triple(x, y, z))
-            block.type = Material.AIR
+            type = Material.AIR
         }
+    }
+
+    /**
+     * Drop the block
+     * @param itemStack blocks to drop
+     */
+    fun Block.Drop(itemStack: ItemStack){
+        //ToDo
     }
 }
